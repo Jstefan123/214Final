959365c5d2ab88afc88669d81c6152cec963e90e
jstefan <jstefan@umich.edu>
2018-12-05 14:00:02 -0500

    reformatted database

--- database.cpp
+++ database.cpp
@@ -1,84 +1,62 @@
-#include "sign.h"
+#include "database.h"
 
-class Database {
+void Database::train() {
+	std::ifstream fin;
+	fin.open("fileName.txt");
 
-private:
+	std::string file;
+	std::string signName;
+	int numImages;
 
-	struct Span {
+	//iterate through the files in the text files and create the appropriate signs and spans
+	while (fin >> file) {
 
-		//name of the sign
-		std::string name;
+		//read in the sign name and number of images
+		signName = file;
+		fin >> numImages;
 
-		//vector of the different varainces of road signs
-		std::vector <Sign> signs;
-        
-        //returns the discrepancy betwen the pixel matrices from projecting the vectors
-        //of query onto the corresponding vectors in this sign in the span
-        double getSpanError(Sign &query) {
+		//loop through the images
+		for (unsigned i = 0; i < numImages; ++i) {
 
-            double totError = 0;
-            // Iterate through the vector of signs
-            for (unsigned i = 0; i < signs.size(); ++i) {
-                // At each sign, add the error
-                totError += query.getError(signs[i]);
-            }
-            return totError / signs.size();
-        }
-	};
+			Sign sign;
 
-	//vector of all the spans of signs
-	std::vector <Span> data;
-
-public:
-
-	void train() {
-		std::ifstream fin;
-		fin.open("fileName.txt");
-
-		std::string file;
-		std::string signName;
-		int numImages;
-
-		//iterate through the files in the text files and create the appropriate signs and spans
-		while (fin >> file) {
-
-			//read in the sign name and number of images
-			signName = file;
-			fin >> numImages;
-
-			//loop through the image text files
+			//loop through the text files for this image
 			for (unsigned i = 0; i < numImages * 3; ++i) {
+
+				//stream that reads the pixel values from a text file
+				std::ifstream pixelRead;
+					
 				fin >> file;
 
 			}
-
 		}
 
 	}
 
-	// Uses the various span errors to predict the road sign of the inputted image
-    std::string predictSign(Sign &query) {
+}
 
-        // Keep track of the minimum span and the element at which it occurred
-        std::string nameMinError = data[0].name;
+// Uses the various span errors to predict the road sign of the inputted image
+std::string Database::predictSign(Sign &query) {
 
-        double minError = data[0].getSpanError;
+    // Keep track of the minimum span and the element at which it occurred
+    std::string nameMinError = data[0].name;
 
-        // Edge case: accounting for when size is 0 to avoid segfault
-        if (data.size() > 0) {
+    double minError = data[0].getSpanError;
 
-            // Iterate through the vector of spans
-            for (unsigned i = 1; i < data.size(); ++i) {
+    // Edge case: accounting for when size is 0 to avoid segfault
+    if (data.size() > 0) {
 
-                // Reset the minimum when necessary
-                if (data[i].getSpanError < minError) {
+        // Iterate through the vector of spans
+        for (unsigned i = 1; i < data.size(); ++i) {
 
-                    minError = data[i].getSpanError;
-                    nameMinError = data[i].name;
-                }
+            // Reset the minimum when necessary
+            if (data[i].getSpanError < minError) {
+
+                minError = data[i].getSpanError;
+                nameMinError = data[i].name;
             }
         }
-        // Return the final minimum
-        return nameMinError;
     }
-};
+    // Return the final minimum
+    return nameMinError;
+}
