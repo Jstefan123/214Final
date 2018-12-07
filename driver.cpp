901718155dadbb52695706f4310f23dd837c1203
jstefan <jstefan@umich.edu>
2018-12-07 15:27:01 -0500

    input redirection

--- driver.cpp
+++ driver.cpp
@@ -5,25 +5,35 @@
 //  Created by John Landy on 12/5/18.
 //
 
+//reads in the text file that has all the test file names from cin
+
 #include "database.h"
 
-int main(int argc, char *argv[]) {
+int main() {
     // Create an instance of the Database class
     Database d;
     // Train using files from within the directory
     d.train();
-    // Read in the filename from the command line
-    std::string filename = argv[1];
-    std::string redIn = filename + "_red.txt";
-    std::string greenIn = filename + "_green.txt";
-    std::string blueIn = filename + "_blue.txt";
-    // Create a Sign object from these files
-    Sign sign;
-    sign.readInImagePixels(redIn, greenIn, blueIn);
-    // Predict the sign from the vector of signs
-    std::string prediction = d.predictSign(sign);
-    std::cout << prediction << std::endl;
 
-	std::cout << argc;
+	std::string testFile;
+
+	//while there are test files to read
+	while (std::cin >> testFile) {
+
+		std::string redIn = testFile + "_red.txt";
+		std::string greenIn = testFile + "_green.txt";
+		std::string blueIn = testFile + "_blue.txt";
+
+		// Create a Sign object from these files
+		Sign sign;
+		sign.readInImagePixels(redIn, greenIn, blueIn);
+
+		// Predict the sign from the vector of signs
+		std::string prediction = d.predictSign(sign);
+		std::cout << "Prediction: " << prediction << '\n';
+
+		std::cout << "Image: stop_sign" << '\n';
+	}
+
 	return 0;
 }
