4de818bb12c30b7f3ac918b4b9665c9965974086
jlandy <jlandy@umich.edu>
2018-12-09 16:45:31 -0500

    New text files

--- Image_Normalization.m
+++ Image_Normalization.m
@@ -3,7 +3,7 @@
     % values of each of the color channels to 3 txt files
 
 % Read in the image
-img = imread('oneway3.jpg');
+img = imread('do_not_enter_test.jpg');
 
 % Resize the image to be 16x16 for simpler processing
 small_img = imresize(img, [16, 16]);
@@ -14,6 +14,6 @@ green = small_img(:,:,2);
 blue = small_img(:,:,3);
 
 % Create the txt files for each color channel
-dlmwrite('oneway_3_red.txt', red, ' ');
-dlmwrite('oneway_3_green.txt', green, ' ');
-dlmwrite('oneway_3_blue.txt', blue, ' ');
+dlmwrite('do_not_enter_test_red.txt', red, ' ');
+dlmwrite('do_not_enter_test_green.txt', green, ' ');
+dlmwrite('do_not_enter_test_blue.txt', blue, ' ');
